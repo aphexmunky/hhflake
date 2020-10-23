ababefdaba9b707fd967cb0bb2909a78dce95491
Josh Hesketh <josh.hesketh@gmail.com>
2020-10-24 00:18:33 +0100

    add token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+contract Token {
+    string public name = "hhflake";
+    string public symbol = "HHF";
+
+    uint256 public totalSupply = 1000000;
+
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
